0e8a9e774c6dd5b57785f39cccf4bc792a3ca7ae
Thomas Rix <tom@rixth.org>
2012-05-23 07:51:45 -0700

    enhanced content script generator

--- lib/tay/cli/generators/content_script.rb
+++ lib/tay/cli/generators/content_script.rb
@@ -10,3 +10,28 @@ module Tay
     end
   end
 end
+
+module Tay
+  module CLI
+    class Generate < ::Thor
+      desc "content_script", "Generate a content script"
+      method_option 'script-name', :type => :string, :default => 'content_script',
+        :aliases => '-n', :banner => 'The name of the content script'
+      method_option 'no-javascript', :type => :boolean, :default => false,
+        :banner => "Don\t create a javascript file"
+      method_option 'no-stylesheet', :type => :boolean, :default => false,
+        :banner => "Don\t create a stylesheet"
+      def content_script
+        fs_name = Utils.filesystem_name(options['script-name'])
+
+        unless options['no-javascript']
+          copy_file('content_script/content_script.js', javascript_dir.join(fs_name + '.js'))
+        end
+        unless options['no-stylesheet']
+          copy_file('content_script/content_script.css', stylesheet_dir.join(fs_name + '.css'))
+        end
+        inject_tayfile_content(render_template('content_script/tayfile', :fs_name => fs_name))
+      end
+    end
+  end
+end
