0f19595c6422a4d3cdd573d248c76854c602f2f1
Thomas Rix <tom@rixth.org>
2012-05-25 23:14:13 -0700

    Remove duplication

--- lib/tay/cli/generators/content_script.rb
+++ lib/tay/cli/generators/content_script.rb
@@ -2,19 +2,6 @@ module Tay
   module CLI
     class Generate < ::Thor
       desc "content_script", "Generate a content script"
-      def content_script
-        copy_file('content_script/content_script.js', javascript_dir.join('content_script.js'))
-        copy_file('content_script/content_script.css', stylesheet_dir.join('content_script.css'))
-        inject_tayfile_content(File.read(find_in_source_paths('content_script/tayfile_content')))
-      end
-    end
-  end
-end
-
-module Tay
-  module CLI
-    class Generate < ::Thor
-      desc "content_script", "Generate a content script"
       method_option 'script-name', :type => :string, :default => 'content_script',
         :aliases => '-n', :banner => 'The name of the content script'
       method_option 'no-javascript', :type => :boolean, :default => false,
